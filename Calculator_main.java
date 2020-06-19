115ce6395947c5bf9bf676bf2d540bad00fdffd4
younis-alaradi <67017967+younis-alaradi@users.noreply.github.com>
2020-06-19 15:36:42 +0300

    Add files via upload

---
+++ Calculator_main.java
@@ -0,0 +1,11 @@
+public class Calculator_main
+{
+    public static void main(String[] args)
+    {
+        Calculator_showing panel = new Calculator_showing();
+
+        panel.setVisible(true);
+
+
+    }
+}
